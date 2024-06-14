12ddb90349c436e95073c3362b5b845a72f4e3fd
Ayush Gharat <gharatayush27@gmail.com>
2024-06-13 22:13:43 -0400

    Fixed one more api route for deployment

--- src/app/api/auth/get-current-user/route.ts
+++ src/app/api/auth/get-current-user/route.ts
@@ -1,8 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import { NextRequest, NextResponse } from "next/server"
+
 
 import {createClient} from "~/utils/supabase/server"
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
     const supabase = await createClient()
     const { data: {user}, error } = await supabase.auth.getUser()
     if (error) {
